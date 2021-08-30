d601f9c39881fed592e7ec64fb42258cc5aebb34
Gideon Etowa <etowagideon@gmail.com>
2021-08-30 22:59:50 +0100

    added mapbox

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const mapContainer = document.getElementById("map-container");
 
 const geedMap = L.map('map-container').setView([51.505, -0.09], 13);
 
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ2VlZDAxIiwiYSI6ImNrc3o2Y21hczJxcGEycXRmY243MWdxa3cifQ.OH12Z9qYxBhjrfNQDQZxog'
+}).addTo(mymap);
