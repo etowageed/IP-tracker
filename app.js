6f1a4a5a6a5499ff712d5449799160f145224630
Gideon Etowa <etowagideon@gmail.com>
2021-08-30 22:20:26 +0100

    added leaflet map

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const inputField = document.getElementById("IPaddress");
 const button = document.getElementById("cta");
 const mapContainer = document.getElementById("map-container");
 
-
+const geedMap = L.map('map-container').setView([51.505, -0.09], 13);
 
